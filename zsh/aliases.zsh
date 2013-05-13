e795a07c4e612f62ffd881faf6eaf1cfccccd1e9
Dave Osborne <osborne.dave@gmail.com>
2013-05-13 17:00:13 -0400

    adding tmux on startup, aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,21 @@
+# base aliases
 alias reload!='. ~/.zshrc'
-alias ls='ls --color -al'
+alias less='less -R' # -R preserves ANSI color escape sequences in output
+alias ls='ls --color -l'
 alias grep='grep --color'
+alias ff='find . -type f -name' # fast find
+alias tmux='tmux -2'
+
+# apt aliases
+alias apti='sudo apt-get install'
+alias apts='sudo apt-cache search'
+alias aptp='sudo apt-cache policy'
+
+# git aliases
+alias gits='git status'
+alias gita='git add'
+alias grm='git rm'
+alias gitc='git commit'
+alias push='git push origin'
+alias pull='git pull'
+alias branch='git checkout'
